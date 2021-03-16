f1fd815c1e919a84b6aa441f039e88eb1ada2f81
GabrielGruit <ggabri432@gmail.com>
2021-03-16 12:04:48 +0100

    Avancement sur le jeu, changement de couleurs suivant la séquence, mais bug qui stoppe la séquence trop tot.

--- app/src/main/java/com/dut2/projetappmobile/MainActivity2.java
+++ app/src/main/java/com/dut2/projetappmobile/MainActivity2.java
@@ -4,6 +4,7 @@ package com.dut2.projetappmobile;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -57,64 +58,30 @@ public class MainActivity2 extends AppCompatActivity {
                 //Toast toast= Toast.makeText(MainActivity2.this,"le chiffre :"+sequence, Toast.LENGTH_LONG);
                 //toast.show();
 
-                for(int i=0; i<nbtour;i++)
+                firstVisibility(0);
+                for(int i=1; i<nbtour;i++)
                 {
                     //Toast chiffre= Toast.makeText(MainActivity2.this,""+sequence.charAt(i), Toast.LENGTH_LONG);
                     //chiffre.show();
-                    Timer rotationTimer = new Timer(true);
 
-                    TimerTask vert = new TimerTask() {
+                    Handler handler = new Handler();
+                    Handler handler2 = new Handler();
+
+                    int finalI = i;
+                    handler.postDelayed(new Runnable(){
                         @Override
-                        public void run() {
-                            greenbutton.setImageAlpha(100);
+                        public void run(){
+                            firstVisibility(finalI);
                         }
-                    };
-                    TimerTask rouge = new TimerTask() {
+                    },1000);
+
+
+                    handler2.postDelayed(new Runnable(){
                         @Override
-                        public void run() {
-                            redbutton.setImageAlpha(100);
+                        public void run(){
+                            secondVisibility();
                         }
-                    };
-                    TimerTask orange = new TimerTask() {
-                        @Override
-                        public void run() {
-                            orangebutton.setImageAlpha(100);
-                        }
-                    };
-                    TimerTask bleu = new TimerTask() {
-                        @Override
-                        public void run() {
-                            bluebutton.setImageAlpha(100);
-                        }
-                    };
-
-
-                    switch (sequence.charAt(i))
-                    {
-
-                        case '1'://vert
-                            rotationTimer.schedule(vert,1000);
-                            break;
-
-                        case '2'://rouge
-                            rotationTimer.schedule(rouge,1000);
-                            break;
-
-                        case '3'://orange
-                            rotationTimer.schedule(orange,1000);
-                            break;
-
-                        case '4'://bleu
-                            rotationTimer.schedule(bleu,1000);
-                            break;
-                    }
-
-                    //Thread.sleep(1000);
-
-                    greenbutton.setImageAlpha(255);
-                    bluebutton.setImageAlpha(255);
-                    orangebutton.setImageAlpha(255);
-                    redbutton.setImageAlpha(255);
+                    },800);
 
 
                 }
@@ -122,6 +89,42 @@ public class MainActivity2 extends AppCompatActivity {
         });
 
 
+    }
+    private void firstVisibility(int i){
+        switch (sequence.charAt(i))
+        {
+            case '1'://vert
+                greenbutton.setImageAlpha(150);
+                break;
+
+            case '2'://rouge
+                redbutton.setImageAlpha(150);
+                break;
+
+            case '3'://orange
+                orangebutton.setImageAlpha(150);
+                break;
+
+            case '4'://bleu
+                bluebutton.setImageAlpha(150);
+                break;
+        }
+    }
+    private void secondVisibility(){
+
+        //Toast alpha= Toast.makeText(MainActivity2.this,""+greenbutton.getAlpha(), Toast.LENGTH_LONG);
+        //alpha.show();
+        //if(greenbutton.getAlpha()==255)
+            greenbutton.setImageAlpha(255);
+
+        //if(bluebutton.getAlpha()==255)
+            bluebutton.setImageAlpha(255);
+
+        //if(orangebutton.getAlpha()==255)
+            orangebutton.setImageAlpha(255);
+
+        //if(redbutton.getAlpha()==255)
+            redbutton.setImageAlpha(255);
     }
 
 }
