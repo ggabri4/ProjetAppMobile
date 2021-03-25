8a4642e01d96423474b2e41ebac4ad5f30908158
GabrielGruit <ggabri432@gmail.com>
2021-03-25 14:13:16 +0100

    Jeux fini ( sans le score )

--- app/src/main/java/com/dut2/projetappmobile/GameActivity.java
+++ app/src/main/java/com/dut2/projetappmobile/GameActivity.java
@@ -36,11 +36,13 @@ public class GameActivity extends AppCompatActivity {
     private Button beginbutton;
     private String sequence;
 
-    private int nbtour=5;
-    private int incr=0;
+    private int nbtour=3;
+    private int incr;
     private char val = '0';
     private boolean click=false;
     private int vie=0;
+    private int viemax;
+    private int etape=0;
 
 
 
@@ -58,96 +60,105 @@ public class GameActivity extends AppCompatActivity {
         uncoeur = findViewById(R.id.un_coeur);
         deuxcoeur = findViewById(R.id.deux_coeur);
 
-
+        //Initialisation debut page
         String chars = "1234";
-
         sequence = "";
-        for(int x=0;x<nbtour;x++)
+        for(int x=0;x<nbtour+1;x++)//+1 pour éviter le out of range
         {
             int i =  (int)(Math.random() * ((3) + 1));
             sequence += chars.charAt(i);
         }
         System.out.println(sequence);
+        viemax=2;
         gametext.setText("Clique sur Démarrer pour lancer !");
 
         beginbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
 
+                //Initialisation clique bouton
                 ImageView nombouton;
                 boolean error=false;
                 click=false;
                 vie=0;
+                incr=0;
+                etape=0;
+                val=0;
                 uncoeur.clearColorFilter();
                 deuxcoeur.clearColorFilter();
 
-                gametext.setText("Retiens bien l'ordre d'allumage !");
-
-                for (incr=0;incr<nbtour;incr++)
-                {
-                    ControleSequence(incr);
-                }
-
-                incr=0;
-
-                //while(!error && incr<nbtour)
-                //{
-                CkeckColor(incr);
-                //}
+                ControleSequence(0);
+                CkeckColor();
 
                 System.out.println("Fin");
             }
         });
 
 
-
     }
     //---------------------Fonction--et--procédure------------------------------
-    private void CkeckColor(int i){
+    private void CkeckColor(){
         ColorMatrix cm = new ColorMatrix();
         cm.setSaturation(0);
         final ColorFilter filter = new ColorMatrixColorFilter(cm);
 
         ButtonListener();
-        if(i>nbtour-1)
+        if(etape>=nbtour-1 && incr==etape && etape!=0)
         {
-            if(vie<2)
+            if(vie<viemax)
                 gametext.setText("\t\t\t\t\t\t\t\tTu as gagné !"+"\n\nClique sur démarrer pour rejouer.");
             else
                 gametext.setText("\t\t\t\t\t\t\t\tTu as perdu."+"\n\nClique sur démarrer pour rejouer.");
         }
-        else if(val == sequence.charAt(i) && vie<2)
+        else if(val == sequence.charAt(incr) && vie<viemax)
         {
             System.out.println("correct");
-            System.out.println("val ="+val+" ,sequence(i) ="+ sequence.charAt(i));
-            System.out.println("incr ="+incr);
-            if(i==nbtour-1)
+            System.out.println("val ="+val+" ,sequence(i) ="+ sequence.charAt(incr));
+            System.out.println("incr ="+incr+" ,etape ="+etape);
+
+            if(etape>=nbtour)
                 gametext.setText("\t\t\t\t\t\t\t\tTu as gagné !"+"\n\nClique sur démarrer pour rejouer.");
             else
-             {
+            {
                 gametext.setText("Bien joué ! Continues");
-                incr++;
-             }
+                if(incr==etape && etape <nbtour-1)
+                {
+                    incr=0;
+                    etape++;
+                    ControleSequence(etape);
+                    System.out.println("etape++");
+                }else if(etape<nbtour) {
+                    incr++;
+                    System.out.println("incr++");
+                }else{
+                    etape++;System.out.println("error");//Gestion de problèmes
+                }
+
+            }
         }
-        else if (click && vie<2)
+        else if (click && vie<viemax && etape <nbtour)
         {
 
             System.out.println("incorrect");
-            System.out.println("val ="+val+" ,sequence(i) ="+ sequence.charAt(i));
-            System.out.println("incr ="+incr);
+            System.out.println("val ="+val+" ,sequence(i) ="+ sequence.charAt(incr));
+            System.out.println("incr ="+incr+" ,etape ="+etape);
             vie++;
 
-            if(vie==2)
+            if(vie==viemax && etape <nbtour)
             {
                 gametext.setText("\t\t\t\t\t\t\t\tTu as perdu."+"\n\nClique sur démarrer pour rejouer.");
                 deuxcoeur.setColorFilter(filter);
-            }else
+            }else if(etape <nbtour)
             {
+                incr=0;
                 gametext.setText("Raté ! -"+vie+" vie");
                 uncoeur.setColorFilter(filter);
-            }
+                ControleSequence(etape);
+            }else
+                System.out.println("NON");
+
         }
-        else {System.out.println("pas clické");}
+        else {System.out.println("Redemarrer");}
 
     }
 
@@ -160,7 +171,7 @@ public class GameActivity extends AppCompatActivity {
                 val = '1';
                 click=true;
                 ButtonClick(greenbutton);
-                CkeckColor(incr);
+                CkeckColor();
             }});
         redbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -168,7 +179,7 @@ public class GameActivity extends AppCompatActivity {
                 val = '2';
                 click=true;
                 ButtonClick(redbutton);
-                CkeckColor(incr);
+                CkeckColor();
             }});
         orangebutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -176,7 +187,7 @@ public class GameActivity extends AppCompatActivity {
                 val = '3';
                 click=true;
                 ButtonClick(orangebutton);
-                CkeckColor(incr);
+                CkeckColor();
             }});
         bluebutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -184,80 +195,55 @@ public class GameActivity extends AppCompatActivity {
                 val = '4';
                 click=true;
                 ButtonClick(bluebutton);
-                CkeckColor(incr);
+                CkeckColor();
             }});
         //}
     }
 
-    private void ControleSequence(int i){
+    private void ControleSequence(int j){
         Handler handler = new Handler();
-
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                handler.postDelayed(new Runnable(){
-                    @Override
-                    public void run(){
-                        firstVisibility(i);
-                    }
-                },(i)*1000);
-
-
-
-                handler.postDelayed(new Runnable(){
-                    @Override
-                    public void run(){
-                        secondVisibility(i);
-                        if(i==nbtour-1)gametext.setText("A toi de jouer !");
-                    }
-                },(i +1)*1000);
-            }
-        },(i+1)*1000+100);
-    }
-
-    private void firstVisibility(int i){
-        switch (sequence.charAt(i))
+        gametext.setText("Retiens bien l'ordre d'allumage !");
+        for (int i=0;i<j+1;i++)
         {
-            case '1'://vert
-                greenbutton.setImageAlpha(150);
-                System.out.println("vert");
-                break;
-
-            case '2'://rouge
-                redbutton.setImageAlpha(150);
-                System.out.println("rouge");
-                break;
-
-            case '3'://orange
-                orangebutton.setImageAlpha(150);
-                System.out.println("orange");
-                break;
-
-            case '4'://bleu
-                bluebutton.setImageAlpha(150);
-                System.out.println("bleu");
-                break;
+            int finalI = i;
+            handler.postDelayed(new Runnable(){
+                @Override
+                public void run(){
+                    handler.postDelayed(new Runnable(){
+                        @Override
+                        public void run(){
+                            Visibility(finalI, 150);
+                        }
+                    },(finalI)*1000);
+                    handler.postDelayed(new Runnable(){
+                        @Override
+                        public void run(){
+                            Visibility(finalI, 255);
+                            if(finalI ==etape)gametext.setText("A toi de jouer !");
+                        }
+                    },(finalI +1)*1000);
+                }
+            },(i+1)*1000+100);
         }
     }
 
-    private void secondVisibility(int i){
-
+    private void Visibility(int i, int alpha){
         switch (sequence.charAt(i))
         {
             case '1'://vert
-                greenbutton.setImageAlpha(255);
+                greenbutton.setImageAlpha(alpha);
                 break;
 
             case '2'://rouge
-                redbutton.setImageAlpha(255);
+                redbutton.setImageAlpha(alpha);
                 break;
 
             case '3'://orange
-                orangebutton.setImageAlpha(255);
+                orangebutton.setImageAlpha(alpha);
                 break;
 
             case '4'://bleu
-                bluebutton.setImageAlpha(255);
+                bluebutton.setImageAlpha(alpha);
                 break;
         }
     }
